7ff42d61050c8164884eb123f467b10642b3aa9f
Artem <artem.sivokoz@gmail.com>
2023-03-23 11:15:14 +0300

    Design V1.0

---
+++ app/src/main/java/com/example/weather_crypto_app/City_Weather.kt
@@ -0,0 +1,23 @@
+package com.example.weather_crypto_app
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [City_Weather.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class City_Weather : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_city__weather, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/weather_crypto_app/MainMenu.kt
@@ -0,0 +1,29 @@
+package com.example.weather_crypto_app
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.fragment_main_menu.*
+
+
+class MainMenu : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_menu, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        weather_choose.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_mainMenu_to_city_Weather2)
+        }
+    }
+
+}

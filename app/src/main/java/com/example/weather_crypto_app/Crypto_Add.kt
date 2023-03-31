9c5155c8d56686a3018174cd4fa8649957c181a4
Artem <artem.sivokoz@gmail.com>
2023-03-31 12:01:40 +0300

    Not working API

--- app/src/main/java/com/example/weather_crypto_app/Crypto_Add.kt
+++ app/src/main/java/com/example/weather_crypto_app/Crypto_Add.kt
@@ -1,17 +1,24 @@
 package com.example.weather_crypto_app
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.weather_crypto_app.data.CryptoApi
 import com.example.weather_crypto_app.data.ManagerCryptoApi
 import com.example.weather_crypto_app.models.CryptoAddModel
 import com.example.weather_crypto_app.models.MainMenuModel
 import com.example.weather_crypto_app.models.MainMenuModules
 import com.example.weather_crypto_app.presentation.ui.adapters.CryptoAddAdapter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class Crypto_Add : Fragment() {
@@ -29,7 +36,25 @@ class Crypto_Add : Fragment() {
 
         //val adapter = CryptoAddAdapter(managerCryptoApi.GetDataCrypto())
 
-        val adapter = CryptoAddAdapter(testAddCrypto())
+        val data = arrayListOf<CryptoAddModel>()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.coingecko.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val cryptoApi = retrofit.create(CryptoApi::class.java)
+        CoroutineScope(Dispatchers.IO).launch {
+            data.add(CryptoAddModel("", "Bitcoin", false))
+            val infoCrypto = cryptoApi.getCrypto()
+            data.add(CryptoAddModel("", "Bitcoin", false))
+            for (coin in infoCrypto) {
+                data.add(CryptoAddModel("", coin.name, false))
+            }
+        }
+        data.add(CryptoAddModel("", "Bitcoin", false))
+        data.add(CryptoAddModel("", "Bitcoin", false))
+
+
+        val adapter = CryptoAddAdapter(data)
 
         recyclerView = view.findViewById(R.id.rv_crypto_add)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
@@ -41,4 +66,25 @@ class Crypto_Add : Fragment() {
         items.add(CryptoAddModel("", "Bitcoin", false))
         return items
     }
+/*
+    private fun getDataCrypto(): List<CryptoAddModel>{
+        val data = arrayListOf<CryptoAddModel>()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.coingecko.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        val cryptoApi = retrofit.create(CryptoApi::class.java)
+        CoroutineScope(Dispatchers.IO).launch {
+            data.add(CryptoAddModel("", "Bitcoin", false))
+            val infoCrypto = cryptoApi.getCrypto()
+            data.add(CryptoAddModel("", "Bitcoin", false))
+            infoCrypto.forEach {
+                data.add(CryptoAddModel("", it.name, false))
+                data.add(CryptoAddModel("", "Bitcoin", false))
+            }
+        }
+        data.add(CryptoAddModel("", "Bitcoin", false))
+        data.add(CryptoAddModel("", "Bitcoin", false))
+        return data
+    } */
 }
--- app/src/main/java/com/example/weather_crypto_app/data/CryptoApi.kt
+++ app/src/main/java/com/example/weather_crypto_app/data/CryptoApi.kt
@@ -1,9 +1,11 @@
 package com.example.weather_crypto_app.data
 
 import com.example.weather_crypto_app.models.crypto.cryptoRep
+import com.example.weather_crypto_app.models.crypto.cryptoRepItem
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface CryptoApi {
     @GET("api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&")
-    suspend fun getCrypto(): cryptoRep
+    suspend fun getCrypto(): List<cryptoRepItem>
 }

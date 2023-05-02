033a4ffad8b5fc1fe405f6d04ff7f18d395c63e8
Artem <artem.sivokoz@gmail.com>
2023-05-02 18:00:38 +0300

    #9 Cleaning code

--- app/src/main/java/com/example/weather_crypto_app/CryptoAdd.kt
+++ app/src/main/java/com/example/weather_crypto_app/CryptoAdd.kt
@@ -25,8 +25,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import kotlin.math.floor
 
 
